455edbfb5d1cba588a041ebeb7006982d171f388
Elian Doran <contact@eliandoran.me>
2026-01-07 23:31:28 +0200

    chore(server): remove runtime from login

--- apps/client/src/login.ts
+++ apps/client/src/login.ts
@@ -1,3 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 // @ts-ignore - module = undefined
 // Required for correct loading of scripts in Electron
 if (typeof module === 'object') {window.module = module; module = undefined;}
+
+document.body.style.display = "block";
