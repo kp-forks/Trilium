a9218960e964dcb7baf7da68858ebe3129f43dc7
Elian Doran <contact@eliandoran.me>
2026-01-24 10:40:16 +0200

    fix(web-clipper): the storage API will not work with a temporary addon ID

--- apps/web-clipper/wxt.config.js
+++ apps/web-clipper/wxt.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
             "<all_urls>",
             "storage",
             "contextMenus"
-        ]
+        ],
+        browser_specific_settings: {
+            gecko: {
+                id: "{1410742d-b377-40e7-a9db-63dc9c6ec99c}"
+            }
+        }
     }
 });
