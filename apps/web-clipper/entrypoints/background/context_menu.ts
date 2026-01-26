810563b3f998cfa6b9721f72d75ce9ad6890fa7b
Elian Doran <contact@eliandoran.me>
2026-01-26 19:21:19 +0200

    fix(web-clipper): duplicate context menu error

---
+++ apps/web-clipper/entrypoints/background/context_menu.ts
@@ -0,0 +1,42 @@
+const CONTEXT_MENU_ITEMS: Browser.contextMenus.CreateProperties[] = [
+    {
+        id: "trilium-save-selection",
+        title: "Save selection to Trilium",
+        contexts: ["selection"]
+    },
+    {
+        id: "trilium-save-cropped-screenshot",
+        title: "Crop screen shot to Trilium",
+        contexts: ["page"]
+    },
+    {
+        id: "trilium-save-whole-screenshot",
+        title: "Save whole screen shot to Trilium",
+        contexts: ["page"]
+    },
+    {
+        id: "trilium-save-page",
+        title: "Save whole page to Trilium",
+        contexts: ["page"]
+    },
+    {
+        id: "trilium-save-link",
+        title: "Save link to Trilium",
+        contexts: ["link"]
+    },
+    {
+        id: "trilium-save-image",
+        title: "Save image to Trilium",
+        contexts: ["image"]
+    }
+];
+
+export default function setupContextMenu() {
+    // Context menu items need to be registered only once.
+    // https://stackoverflow.com/questions/64318529/cannot-create-item-with-duplicate-context-menu-id-in-extension
+    browser.runtime.onInstalled.addListener(() => {
+        for (const item of CONTEXT_MENU_ITEMS) {
+            browser.contextMenus.create(item);
+        }
+    });
+}
