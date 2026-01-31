a45fb975c09e6b71c413b5810d0004dd75d46571
Elian Doran <contact@eliandoran.me>
2026-01-31 15:53:19 +0200

    chore(mobile/tab_switcher): add button in launch bar

---
+++ apps/client/src/widgets/mobile_widgets/TabSwitcher.tsx
@@ -0,0 +1,15 @@
+import { LaunchBarActionButton } from "../launch_bar/launch_bar_widgets";
+
+export default function TabSwitcher() {
+    return (
+        <>
+            <LaunchBarActionButton
+                icon="bx bx-rectangle"
+                text="Tabs"
+                onClick={() => {
+
+                }}
+            />
+        </>
+    );
+}
