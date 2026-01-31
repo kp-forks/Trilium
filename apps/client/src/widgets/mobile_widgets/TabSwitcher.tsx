3a9b448a833ed958a14c0789e3798fb0bb9fe90b
Elian Doran <contact@eliandoran.me>
2026-01-31 15:59:38 +0200

    chore(mobile/tab_switcher): create empty modal

--- apps/client/src/widgets/mobile_widgets/TabSwitcher.tsx
+++ apps/client/src/widgets/mobile_widgets/TabSwitcher.tsx
@@ -1,15 +1,40 @@
+import "./TabSwitcher.css";
+
+import { createPortal } from "preact/compat";
+import { useState } from "preact/hooks";
+
 import { LaunchBarActionButton } from "../launch_bar/launch_bar_widgets";
+import Modal from "../react/Modal";
 
 export default function TabSwitcher() {
+    const [ shown, setShown ] = useState(false);
+
     return (
         <>
             <LaunchBarActionButton
                 icon="bx bx-rectangle"
                 text="Tabs"
-                onClick={() => {
-
-                }}
+                onClick={() => setShown(true)}
             />
+            {createPortal(<TabBarModal shown={shown} setShown={setShown} />, document.body)}
         </>
+    );
+}
+
+
+function TabBarModal({ shown, setShown }: {
+    shown: boolean;
+    setShown: (newValue: boolean) => void;
+}) {
+    return (
+        <Modal
+            className="tab-bar-modal"
+            size="xl"
+            title="Tabs"
+            show={shown}
+            onHidden={() => setShown(false)}
+        >
+            Hi
+        </Modal>
     );
 }
