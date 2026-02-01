c36ce3ea143df67a17b4275846652777503d8d91
Elian Doran <contact@eliandoran.me>
2026-02-01 22:29:43 +0200

    fix(mobile/note_actions): insert child note not working

--- apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
+++ apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
@@ -1,9 +1,10 @@
 import { t } from "../../services/i18n";
 import { getHelpUrlForNote } from "../../services/in_app_help";
+import note_create from "../../services/note_create";
 import { openInAppHelpFromUrl } from "../../services/utils";
 import { FormDropdownDivider, FormListItem } from "../react/FormList";
 import { useNoteContext } from "../react/hooks";
-import { CommandItem, NoteContextMenu } from "../ribbon/NoteActions";
+import { NoteContextMenu } from "../ribbon/NoteActions";
 
 export default function MobileDetailMenu() {
     const { note, noteContext, parentComponent, ntxId } = useNoteContext();
@@ -17,7 +18,10 @@ export default function MobileDetailMenu() {
                 <NoteContextMenu
                     note={note} noteContext={noteContext}
                     extraItems={<>
-                        <CommandItem command="insertChildNote" icon="bx bx-plus" disabled={note.type === "search"} text={t("mobile_detail_menu.insert_child_note")} />
+                        <FormListItem
+                            onClick={() => noteContext?.notePath && note_create.createNote(noteContext.notePath)}
+                            icon="bx bx-plus"
+                        >{t("mobile_detail_menu.insert_child_note")}</FormListItem>
                         {helpUrl && <>
                             <FormDropdownDivider />
                             <FormListItem
