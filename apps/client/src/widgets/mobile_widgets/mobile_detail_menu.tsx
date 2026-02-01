4c72d8691be785b220ac0b928af8b98158c12292
Elian Doran <contact@eliandoran.me>
2026-02-01 21:45:43 +0200

    fix(mobile/note_actions): reintroduce help button

--- apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
+++ apps/client/src/widgets/mobile_widgets/mobile_detail_menu.tsx
@@ -1,10 +1,13 @@
 import { t } from "../../services/i18n";
-import { FormDropdownDivider } from "../react/FormList";
+import { getHelpUrlForNote } from "../../services/in_app_help";
+import { openInAppHelpFromUrl } from "../../services/utils";
+import { FormDropdownDivider, FormListItem } from "../react/FormList";
 import { useNoteContext } from "../react/hooks";
 import { CommandItem, NoteContextMenu } from "../ribbon/NoteActions";
 
 export default function MobileDetailMenu() {
     const { note, noteContext } = useNoteContext();
+    const helpUrl = getHelpUrlForNote(note);
 
     return (
         <div style={{ contain: "none" }}>
@@ -13,6 +16,11 @@ export default function MobileDetailMenu() {
                     note={note} noteContext={noteContext}
                     extraItems={<>
                         <CommandItem command="insertChildNote" icon="bx bx-plus" disabled={note.type === "search"} text={t("mobile_detail_menu.insert_child_note")} />
+                        <FormDropdownDivider />
+                        {helpUrl && <FormListItem
+                            icon="bx bx-help-circle"
+                            onClick={() => openInAppHelpFromUrl(helpUrl)}
+                        >{t("help-button.title")}</FormListItem>}
                         <FormDropdownDivider />
                     </>}
                 />
