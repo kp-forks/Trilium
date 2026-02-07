d4b16fcdd19ce10e197bcc2f84eb8f6fc161ccbc
Elian Doran <contact@eliandoran.me>
2026-02-07 10:36:00 +0200

    feat(website/resources): display icon packs

--- apps/website/src/pages/Resources/Resources.tsx
+++ apps/website/src/pages/Resources/Resources.tsx
@@ -1,12 +1,30 @@
 import { useTranslation } from "react-i18next";
 
+import Card from "../../components/Card";
+import Section from "../../components/Section";
 import { usePageTitle } from "../../hooks";
+
+const iconPacksMeta = Object.values(import.meta.glob("../../assets/resources/icon-packs/*.json", {
+    eager: true
+}));
 
 export default function Resources() {
     const { t } = useTranslation();
     usePageTitle(t("resources.title"));
 
     return (
-        <p>Resources go here.</p>
+        <Section>
+            <h2>{t("resources.icon_packs")}</h2>
+
+            <div className="grid-3-cols">
+                {iconPacksMeta.map(meta => (
+                    <Card
+                        key={meta.name}
+                        title={`${meta.name} ${meta.version}`}
+                        moreInfoUrl={meta.website}
+                    />
+                ))}
+            </div>
+        </Section>
     );
 }
