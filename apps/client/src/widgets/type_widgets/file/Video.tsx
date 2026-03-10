4c73f31aca7123fbf6084e144b5c567d8dd6198b
Elian Doran <contact@eliandoran.me>
2026-03-10 18:54:53 +0200

    feat(video_player): start adding custom controls (play/pause)

--- apps/client/src/widgets/type_widgets/file/Video.tsx
+++ apps/client/src/widgets/type_widgets/file/Video.tsx
@@ -1,15 +1,27 @@
 import "./Video.css";
 
+import { useRef } from "preact/hooks";
+
 import FNote from "../../../entities/fnote";
 import { getUrlForDownload } from "../../../services/open";
+import ActionButton from "../../react/ActionButton";
 
 export default function VideoPreview({ note }: { note: FNote }) {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
     return (
-        <video
-            class="video-preview"
-            src={getUrlForDownload(`api/notes/${note.noteId}/open-partial`)}
-            datatype={note?.mime}
-            controls
-        />
+        <div className="video-preview-wrapper">
+            <video
+                ref={videoRef}
+                class="video-preview"
+                src={getUrlForDownload(`api/notes/${note.noteId}/open-partial`)}
+                datatype={note?.mime}
+            />
+
+            <div className="video-preview-controls">
+                <ActionButton icon="bx bx-play" text="Play" onClick={() => videoRef.current?.play()} />
+                <ActionButton icon="bx bx-pause" text="Pause" onClick={() => videoRef.current?.pause()} />
+            </div>
+        </div>
     );
 }
