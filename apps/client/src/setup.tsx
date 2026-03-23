939f93180949c46a5d4d1ce1cbbb36d81654f5ea
Elian Doran <contact@eliandoran.me>
2026-03-23 19:20:30 +0200

    chore(standalone/setup): setup translation partially

--- apps/client/src/setup.tsx
+++ apps/client/src/setup.tsx
@@ -1,13 +1,19 @@
 import { render } from "preact";
 
+import { initLocale, t } from "./services/i18n";
+
 async function main() {
+    await initLocale();
+
     const bodyWrapper = document.createElement("div");
     render(<App />, bodyWrapper);
     document.body.appendChild(bodyWrapper);
 }
 
 function App() {
-    return <p>Loading...</p>;
+    return (
+        <h1>{t("setup.heading")}</h1>
+    );
 }
 
 main();
