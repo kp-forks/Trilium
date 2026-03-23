12347d5c4a4b461fc964a35e2a3ece8c4d9e3196
Elian Doran <contact@eliandoran.me>
2026-03-23 19:30:00 +0200

    chore(standalone/setup): basic layout

--- apps/client/src/setup.tsx
+++ apps/client/src/setup.tsx
@@ -1,18 +1,34 @@
+import "./setup.css";
+
 import { render } from "preact";
 
 import { initLocale, t } from "./services/i18n";
+import Button from "./widgets/react/Button";
 
 async function main() {
     await initLocale();
 
     const bodyWrapper = document.createElement("div");
+    bodyWrapper.classList.add("setup-body-wrapper");
     render(<App />, bodyWrapper);
     document.body.replaceChildren(bodyWrapper);
 }
 
 function App() {
     return (
-        <h1>{t("setup.heading")}</h1>
+        <>
+            <h1>{t("setup.heading")}</h1>
+
+            <div class="setup-options">
+                <Button
+                    text={t("setup.new-document")}
+                />
+
+                <Button
+                    text={t("setup.sync-from-server")}
+                />
+            </div>
+        </>
     );
 }
 
