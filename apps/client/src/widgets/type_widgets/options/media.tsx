ba91d91fd17fdecdf7501a6531d43a71a0dc7096
Elian Doran <contact@eliandoran.me>
2026-04-01 22:34:39 +0300

    chore(options): start adding options for OCR

--- apps/client/src/widgets/type_widgets/options/media.tsx
+++ apps/client/src/widgets/type_widgets/options/media.tsx
@@ -1,11 +1,20 @@
 import { t } from "../../../services/i18n";
-import { FormTextBoxWithUnit } from "../../react/FormTextBox";
+import FormTextBox, { FormTextBoxWithUnit } from "../../react/FormTextBox";
 import FormToggle from "../../react/FormToggle";
 import { useTriliumOption, useTriliumOptionBool } from "../../react/hooks";
 import OptionsRow from "./components/OptionsRow";
 import OptionsSection from "./components/OptionsSection";
 
 export default function MediaSettings() {
+    return (
+        <>
+            <ImageSettings />
+            <OcrSettings />
+        </>
+    );
+}
+
+function ImageSettings() {
     const [ downloadImagesAutomatically, setDownloadImagesAutomatically ] = useTriliumOptionBool("downloadImagesAutomatically");
     const [ compressImages, setCompressImages ] = useTriliumOptionBool("compressImages");
     const [ imageMaxWidthHeight, setImageMaxWidthHeight ] = useTriliumOption("imageMaxWidthHeight");
@@ -44,6 +53,52 @@ export default function MediaSettings() {
                     disabled={!compressImages}
                     unit={t("units.percentage")}
                     currentValue={imageJpegQuality} onChange={setImageJpegQuality}
+                />
+            </OptionsRow>
+        </OptionsSection>
+    );
+}
+
+function OcrSettings() {
+    const [ ocrEnabled, setOcrEnabled ] = useTriliumOptionBool("ocrEnabled");
+    const [ ocrAutoProcess, setOcrAutoProcess ] = useTriliumOptionBool("ocrAutoProcessImages");
+    const [ ocrLanguage, setOcrLanguage ] = useTriliumOption("ocrLanguage");
+    const [ ocrMinConfidence, setOcrMinConfidence ] = useTriliumOption("ocrMinConfidence");
+
+    return (
+        <OptionsSection title={t("images.ocr_section_title")}>
+            <OptionsRow name="ocr-enabled" label={t("images.enable_ocr")} description={t("images.ocr_description")}>
+                <FormToggle
+                    switchOnName="" switchOffName=""
+                    currentValue={ocrEnabled}
+                    onChange={setOcrEnabled}
+                />
+            </OptionsRow>
+
+            <OptionsRow name="ocr-auto-process" label={t("images.ocr_auto_process")} description={t("images.ocr_auto_process_description")}>
+                <FormToggle
+                    switchOnName="" switchOffName=""
+                    currentValue={ocrAutoProcess}
+                    onChange={setOcrAutoProcess}
+                    disabled={!ocrEnabled}
+                />
+            </OptionsRow>
+
+            <OptionsRow name="ocr-language" label={t("images.ocr_language")} description={t("images.ocr_language_description")}>
+                <FormTextBox
+                    disabled={!ocrEnabled}
+                    currentValue={ocrLanguage}
+                    onChange={setOcrLanguage}
+                />
+            </OptionsRow>
+
+            <OptionsRow name="ocr-min-confidence" label={t("images.ocr_min_confidence")} description={t("images.ocr_confidence_description")}>
+                <FormTextBoxWithUnit
+                    type="number" min="0" max="1" step="0.05"
+                    disabled={!ocrEnabled}
+                    unit={t("images.ocr_confidence_unit")}
+                    currentValue={ocrMinConfidence}
+                    onChange={setOcrMinConfidence}
                 />
             </OptionsRow>
         </OptionsSection>
