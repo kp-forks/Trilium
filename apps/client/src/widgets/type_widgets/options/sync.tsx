7c1f509eca1aadbf224c709c158b5ab180aa4535
Elian Doran <contact@eliandoran.me>
2026-04-13 15:56:41 +0300

    chore(options/sync): merge sections and use options row

--- apps/client/src/widgets/type_widgets/options/sync.tsx
+++ apps/client/src/widgets/type_widgets/options/sync.tsx
@@ -6,22 +6,14 @@ import server from "../../../services/server";
 import toast from "../../../services/toast";
 import { openInAppHelpFromUrl } from "../../../services/utils";
 import Button from "../../react/Button";
-import FormGroup from "../../react/FormGroup";
-import FormText from "../../react/FormText";
 import FormTextBox from "../../react/FormTextBox";
 import { useTriliumOptions } from "../../react/hooks";
-import RawHtml from "../../react/RawHtml";
-import OptionsRow from "./components/OptionsRow";
+import OptionsRow, { OptionsRowWithButton } from "./components/OptionsRow";
 import OptionsSection from "./components/OptionsSection";
 import TimeSelector from "./components/TimeSelector";
 
 export default function SyncOptions() {
-    return (
-        <>
-            <SyncConfiguration />
-            <SyncTest />
-        </>
-    );
+    return <SyncConfiguration />;
 }
 
 export function SyncConfiguration() {
@@ -30,7 +22,7 @@ export function SyncConfiguration() {
     const syncProxy = useRef(options.syncProxy);
 
     return (
-        <OptionsSection title={t("sync_2.config_title")}>
+        <OptionsSection>
             <form onSubmit={(e) => {
                 setOptions({
                     syncServerHost: syncServerHost.current,
@@ -38,32 +30,27 @@ export function SyncConfiguration() {
                 });
                 e.preventDefault();
             }}>
-                <FormGroup name="sync-server-host" label={t("sync_2.server_address")}>
+                <OptionsRow name="sync-server-host" label={t("sync_2.server_address")} description={t("sync_2.server_address_description")} stacked>
                     <FormTextBox
                         placeholder="https://<host>:<port>"
                         currentValue={syncServerHost.current} onChange={(newValue) => syncServerHost.current = newValue}
                     />
-                </FormGroup>
+                </OptionsRow>
 
-                <FormGroup name="sync-proxy" label={t("sync_2.proxy_label")}
-                    description={<>
-                        <strong>{t("sync_2.note")}:</strong> {t("sync_2.note_description")}<br/>
-                        <RawHtml html={t("sync_2.special_value_description")} />
-                    </>}
-                >
+                <OptionsRow name="sync-proxy" label={t("sync_2.proxy_label")} description={t("sync_2.proxy_description")} stacked>
                     <FormTextBox
                         placeholder="https://<host>:<port>"
                         currentValue={syncProxy.current} onChange={(newValue) => syncProxy.current = newValue}
                     />
-                </FormGroup>
+                </OptionsRow>
 
-                <div style={{ display: "flex", justifyContent: "spaceBetween"}}>
-                    <Button text={t("sync_2.save")} kind="primary" />
-                    <Button text={t("sync_2.help")} onClick={() => openInAppHelpFromUrl("cbkrhQjrkKrh")} />
-                </div>
+                <OptionsRow name="save-sync-config" centered>
+                    <div style={{ display: "flex", gap: "8px" }}>
+                        <Button text={t("sync_2.save")} kind="primary" />
+                        <Button text={t("sync_2.help")} onClick={() => openInAppHelpFromUrl("cbkrhQjrkKrh")} />
+                    </div>
+                </OptionsRow>
             </form>
-
-            <hr/>
 
             <OptionsRow name="sync-server-timeout" label={t("sync_2.timeout")} description={t("sync_2.timeout_description")}>
                 <TimeSelector
@@ -73,16 +60,10 @@ export function SyncConfiguration() {
                     minimumSeconds={1}
                 />
             </OptionsRow>
-        </OptionsSection>
-    );
-}
 
-export function SyncTest() {
-    return (
-        <OptionsSection title={t("sync_2.test_title")}>
-            <FormText>{t("sync_2.test_description")}</FormText>
-            <Button
-                text={t("sync_2.test_button")}
+            <OptionsRowWithButton
+                label={t("sync_2.test_button")}
+                description={t("sync_2.test_description")}
                 onClick={async () => {
                     const result = await server.post<SyncTestResponse>("sync/test");
 
