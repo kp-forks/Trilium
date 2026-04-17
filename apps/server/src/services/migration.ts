64bc1271e178cde4fb98a4cd01d2e8d7b6934eda
Elian Doran <contact@eliandoran.me>
2026-04-17 23:03:28 +0300

    fix(server): prevent NaN potentially corrupting the backup when migrating

--- apps/server/src/services/migration.ts
+++ apps/server/src/services/migration.ts
@@ -130,6 +130,11 @@ function isDbUpToDate() {
 async function migrateIfNecessary() {
     const currentDbVersion = getDbVersion();
 
+    if (isNaN(currentDbVersion)) {
+        await crash(t("migration.invalid_db_version"));
+        return;
+    }
+
     if (currentDbVersion > appInfo.dbVersion && process.env.TRILIUM_IGNORE_DB_VERSION !== "true") {
         await crash(t("migration.wrong_db_version", { version: currentDbVersion, targetVersion: appInfo.dbVersion }));
     }
