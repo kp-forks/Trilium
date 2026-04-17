18d69f94d2282b54fd2e01f2c6a27766eded4024
Elian Doran <contact@eliandoran.me>
2026-04-17 06:32:49 +0300

    feat(markdown): introduce some padding

--- apps/client/src/widgets/type_widgets/code/Markdown.tsx
+++ apps/client/src/widgets/type_widgets/code/Markdown.tsx
@@ -1,12 +1,24 @@
+import "./Markdown.css";
+
+import { Marked } from "marked";
+import { useMemo, useState } from "preact/hooks";
+
+import { SanitizedHtml } from "../../react/RawHtml";
 import SplitEditor from "../helpers/SplitEditor";
 import { TypeWidgetProps } from "../type_widget";
 
+const marked = new Marked({ breaks: true, gfm: true });
+
 export default function Markdown(props: TypeWidgetProps) {
+    const [ content, setContent ] = useState("");
+    const html = useMemo(() => marked.parse(content) as string, [ content ]);
+
     return (
         <SplitEditor
             noteType="code"
             {...props}
-            previewContent={<div>Hello World</div>}
+            onContentChanged={setContent}
+            previewContent={<SanitizedHtml className="markdown-preview" html={html} />}
         />
     );
 }
